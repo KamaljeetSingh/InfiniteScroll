c353fc9aa41091d4fce5fc2df119a9aa782b9023
Rishabh Sharma <rishabh.sharma@counterpart.com>
2018-08-27 18:33:30 +0530

    commit103

--- src/app/app-monuments/app-monument.component.ts
+++ src/app/app-monuments/app-monument.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-    templateUrl: './app-monument.component.html'
+    templateUrl: './app-monument.component.html',
+    styleUrls: ['./app-monument.component.css']
 })
 export class MonumentComponent{}
