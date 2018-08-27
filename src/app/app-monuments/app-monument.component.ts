ad669f14a5a3e0a1bda95a27ae228d9751fb1303
Rishabh Sharma <rishabh.sharma@counterpart.com>
2018-08-27 18:33:30 +0530

    commit103

--- src/app/app-monuments/app-monument.component.ts
+++ src/app/app-monuments/app-monument.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-    templateUrl: './app-monument.component.html'
+    templateUrl: './app-monument.component.html',
+    styleUrls: ['./app-monument.component.css']
 })
 export class MonumentComponent implements OnInit{
     ngOnInit(){
